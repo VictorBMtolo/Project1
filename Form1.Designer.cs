7f66b62953910e0a2bc7a5eb29cbeefba805b678
VictorBMtolo <Victormtolo6@gmail.com>
2021-08-25 09:57:30 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label contactNumberLabel;
+            System.Windows.Forms.Label customerIDLabel;
+            System.Windows.Forms.Label fullNameLabel;
+            this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.contactNumberTextBox = new System.Windows.Forms.TextBox();
+            this.customerIDTextBox = new System.Windows.Forms.TextBox();
+            this.fullNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            contactNumberLabel = new System.Windows.Forms.Label();
+            customerIDLabel = new System.Windows.Forms.Label();
+            fullNameLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customerBindingSource
+            // 
+            this.customerBindingSource.DataSource = typeof(Project1.Customer);
+            // 
+            // contactNumberLabel
+            // 
+            contactNumberLabel.AutoSize = true;
+            contactNumberLabel.Location = new System.Drawing.Point(45, 110);
+            contactNumberLabel.Name = "contactNumberLabel";
+            contactNumberLabel.Size = new System.Drawing.Size(87, 13);
+            contactNumberLabel.TabIndex = 0;
+            contactNumberLabel.Text = "Contact Number:";
+            // 
+            // contactNumberTextBox
+            // 
+            this.contactNumberTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "ContactNumber", true));
+            this.contactNumberTextBox.Location = new System.Drawing.Point(138, 107);
+            this.contactNumberTextBox.Name = "contactNumberTextBox";
+            this.contactNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.contactNumberTextBox.TabIndex = 1;
+            // 
+            // customerIDLabel
+            // 
+            customerIDLabel.AutoSize = true;
+            customerIDLabel.Location = new System.Drawing.Point(45, 136);
+            customerIDLabel.Name = "customerIDLabel";
+            customerIDLabel.Size = new System.Drawing.Size(68, 13);
+            customerIDLabel.TabIndex = 2;
+            customerIDLabel.Text = "Customer ID:";
+            // 
+            // customerIDTextBox
+            // 
+            this.customerIDTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "CustomerID", true));
+            this.customerIDTextBox.Location = new System.Drawing.Point(138, 133);
+            this.customerIDTextBox.Name = "customerIDTextBox";
+            this.customerIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.customerIDTextBox.TabIndex = 3;
+            // 
+            // fullNameLabel
+            // 
+            fullNameLabel.AutoSize = true;
+            fullNameLabel.Location = new System.Drawing.Point(45, 162);
+            fullNameLabel.Name = "fullNameLabel";
+            fullNameLabel.Size = new System.Drawing.Size(57, 13);
+            fullNameLabel.TabIndex = 4;
+            fullNameLabel.Text = "Full Name:";
+            // 
+            // fullNameTextBox
+            // 
+            this.fullNameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "FullName", true));
+            this.fullNameTextBox.Location = new System.Drawing.Point(138, 159);
+            this.fullNameTextBox.Name = "fullNameTextBox";
+            this.fullNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.fullNameTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(340, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Please fill in Information bellow";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(contactNumberLabel);
+            this.Controls.Add(this.contactNumberTextBox);
+            this.Controls.Add(customerIDLabel);
+            this.Controls.Add(this.customerIDTextBox);
+            this.Controls.Add(fullNameLabel);
+            this.Controls.Add(this.fullNameTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource customerBindingSource;
+        private System.Windows.Forms.TextBox contactNumberTextBox;
+        private System.Windows.Forms.TextBox customerIDTextBox;
+        private System.Windows.Forms.TextBox fullNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            CustomerdataEntities db = new CustomerdataEntities();
+            db.Customers.Add(customerBindingSource.Current as Customer);
+            int result = await db.SaveChangesAsync();
+            if (result > 0)
+                notifyIcon1.ShowBalloonTip(3000, "Message", "Your data has been successfully saved", ToolTipIcon.Info);
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            customerBindingSource.DataSource = new Customer();
+            notifyIcon1.Icon = SystemIcons.Application;
+        }
+    }
+}
